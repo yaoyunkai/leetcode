40453a1eda3668227d8702155b0f737212c66e65
liberty <1052433260@qq.com>
2023-08-29 23:00:20 +0800

    leetcode

---
+++ 150/80 Remove Duplicates from Sorted Array II/sol.py
@@ -0,0 +1,16 @@
+"""
+Created at 2023/8/29
+
+给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+nums = [0,0,1,1,1,1,2,3,3]
+
+"""
+from typing import List
+
+
+class Solution:
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pass
